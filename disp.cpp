0f813d27037598c39d05bbe9b1db51890671ff2d
luftskepp <luftskepp@gmail.com>
2016-09-29 13:54:37 +0200

    fixed indents

--- disp.cpp
+++ disp.cpp
@@ -6,16 +6,16 @@ using namespace cv;
 
 Mat getDisparityMap(Ptr<StereoSGBM> sbm, Mat frameLeft, Mat frameRight){
 	Mat fL, fR, disparityMap;
-   	double minVal, maxVal;
+	double minVal, maxVal;
 	cvtColor(frameLeft, fL, COLOR_BGR2GRAY); //.convertTo(fL, CV_8UC1);
-      	cvtColor(frameRight, fR, COLOR_BGR2GRAY); //.convertTo(fR, CV_8UC1);
-      
-      	// calculate disparity map
-      	sbm->compute(fR, fL, disparityMap);
-      	minMaxLoc( disparityMap, &minVal, &maxVal );
-      
-      	// normalize disparity map
-      	disparityMap.convertTo(disparityMap, CV_8UC1, 255/(maxVal - minVal));
+	cvtColor(frameRight, fR, COLOR_BGR2GRAY); //.convertTo(fR, CV_8UC1);
+
+	// calculate disparity map
+	sbm->compute(fR, fL, disparityMap);
+	minMaxLoc( disparityMap, &minVal, &maxVal );
+
+	// normalize disparity map
+	disparityMap.convertTo(disparityMap, CV_8UC1, 255/(maxVal - minVal));
 
 	return disparityMap;
 }
