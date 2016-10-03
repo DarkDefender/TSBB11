994e155d8c626d717b6e530bc54efe29f3d9920b
luftskepp <luftskepp@gmail.com>
2016-10-03 10:07:10 +0200

    added feature matching

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 
 // include own headers
 #include "disp.h"
+//#include "akazefeature.h"
 
 
 
@@ -48,37 +49,95 @@ int main(int argc, char* argv[]){
 	namedWindow("LeftFrame", CV_WINDOW_AUTOSIZE);
 	namedWindow("RightFrame", CV_WINDOW_AUTOSIZE);
 	namedWindow("disparity", CV_WINDOW_AUTOSIZE);
-	namedWindow("3dpoints", CV_WINDOW_AUTOSIZE);
+	//namedWindow("3dpoints", CV_WINDOW_AUTOSIZE);
+	namedWindow("result", CV_WINDOW_AUTOSIZE);
+	// feature matching
+	vector<KeyPoint> kpts1, kpts2, matched1, matched2, inliers1, inliers2;
+	Mat desc1, desc2;
 
+	Ptr<AKAZE> akaze = AKAZE::create();
+
+	vector<DMatch> goodMatches;
+
+	vector< vector<DMatch> > NNMatches;
+
+    BFMatcher matcher(NORM_HAMMING);
+
+	float nn_match_ratio = 0.8f;
+    int frameCounter = 0;
+    int featureMatchingOn = 0;
+	// fundamental matrix
+	Mat fMatrix; // init to fundamental matrix for rect. stereo. 
 	// main loop
 	while(1){
-
+		frameCounter++;
+        cout << "frame: " << frameCounter << endl;
 		// get next frame
 		capLeft >> frameLeft;
 		capRight >> frameRight;
 
 		if (frameLeft.empty() || frameRight.empty()){
 			cout << "cannot read frame" << endl;
-			break;
+			return -1;
+		}
+		
+		if (featureMatchingOn){
+		// feature matching
+		Mat fL, fR;
+		cvtColor(frameLeft, fL, COLOR_BGR2GRAY);
+		cvtColor(frameRight, fR, COLOR_BGR2GRAY);
+
+		// get descriptors
+		akaze->detectAndCompute(fL, noArray(), kpts1, desc1);
+		akaze->detectAndCompute(fR, noArray(), kpts2, desc2);
+
+		// match points
+		matcher.knnMatch(desc1, desc2, NNMatches, 2);
+
+        for(size_t i = 0; i < NNMatches.size(); i++) {
+			DMatch first = NNMatches[i][0];
+			float dist1 = NNMatches[i][0].distance;
+			float dist2 = NNMatches[i][1].distance;
+
+			if(dist1 < nn_match_ratio * dist2) {
+            matched1.push_back(kpts1[first.queryIdx]);
+            matched2.push_back(kpts2[first.trainIdx]);
+			}
 		}
 
-		disparityMap = getDisparityMap(sbm, frameLeft, frameRight);
-		/*
-		   cvtColor(frameLeft, fL, COLOR_BGR2GRAY); //.convertTo(fL, CV_8UC1);
-		   cvtColor(frameRight, fR, COLOR_BGR2GRAY); //.convertTo(fR, CV_8UC1);
+		// check if points fulfil epipolar geometry (y1'*fMatrix*y2 = 0); 
+		for(unsigned i = 0; i < matched1.size(); i++) {
+			/*Mat col = Mat::ones(3, 1, CV_64F);
+			col.at<double>(0) = matched1[i].pt.x;
+			col.at<double>(1) = matched1[i].pt.y;
 
-		// calculate disparity map
-		sbm->compute(fR, fL, disp_map);
-		minMaxLoc( disp_map, &minVal, &maxVal );
+			col = homography * col;
+			col /= col.at<double>(2);
+			double dist = sqrt( pow(col.at<double>(0) - matched2[i].pt.x, 2) +
+                            pow(col.at<double>(1) - matched2[i].pt.y, 2));
+             */
+			//if(dist < inlier_threshold) {
+				int new_i = static_cast<int>(inliers1.size());
+				inliers1.push_back(matched1[i]);
+				inliers2.push_back(matched2[i]);
+				goodMatches.push_back(DMatch(new_i, new_i, 0));
+			//}
+		}
 
-		// normalize disparity map
-		disp_map.convertTo(frame_disp, CV_8UC1, 255/(maxVal - minVal));
-		*/
+		Mat res;
+		drawMatches(fL, inliers1, fR, inliers2, goodMatches, res);
+        imshow("result", res);
+		inliers1.clear();
+		inliers2.clear();
+			goodMatches.clear();
+			matched1.clear();
+			matched2.clear();
+		}
+        disparityMap = getDisparityMap( sbm , frameLeft, frameRight);
 		// show frames
 		imshow("disparity", disparityMap);
-		imshow("LeftFrame", frameLeft);
 		imshow("RightFrame", frameRight);
-
+		imshow("LeftFrame", frameLeft);
 		// wait for draw and quit on esc
 		if(waitKey(30) == 27){
 			cout << "user escape" << endl;
