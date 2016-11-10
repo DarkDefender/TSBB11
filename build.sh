7265210bf300217399f7d3ffbc3350e24815065c
Sebastian Parborg <darkdefende@gmail.com>
2016-11-10 10:58:11 +0100

    added build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd 3drecon
+cmake ./OpenCVReprojectImageToPointCloud
+make
+cd ..
+
+cd ORB_SLAM2
+sh build.sh
+cd ..
+
+cd spsstereo
+mkdir build
+cd build
+cmake ../
+make
+cd ../../
+
+mkdir build
+cd build
+cmake ../
+
