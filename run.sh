546af9d9992fbdb285f399b0cc66d1550b93022f
luftskepp <luftskepp@gmail.com>
2016-11-10 15:08:46 +0100

    updated run script and spsstereo

--- run.sh
+++ run.sh
@@ -11,32 +11,32 @@ echo $1
 echo $2
 echo $3
 
-exit 0
+#exit 0
 
 #Preprocess input image data
 
-mkdir -p data/left
-mkdir -p data/right
-
-ffmpeg -i $1 data/left/%04d.png
-
-ffmpeg -i $2 data/right/%04d.png
-
-for filename in data/left/*.png; do
-	base=$(basename $filename) 
-	echo ${base%.*} >> data/timestamps.txt
-done
-
+#mkdir -p data/left
+#mkdir -p data/right
+#
+#ffmpeg -i $1 data/left/%04d.png
+#
+#ffmpeg -i $2 data/right/%04d.png
+#
+#for filename in data/left/*.png; do
+#	base=$(basename $filename) 
+#	echo ${base%.*} >> data/timestamps.txt
+#done
+#
 #Run orb slam 2 to extract camera path
 
-cd ORB_SLAM2
+#cd ORB_SLAM2
 
-./Examples/Stereo/saab_stereo Vocabulary/ORBvoc.txt Examples/Stereo/SAAB.yaml ../data/left ../data/right ../data/timestamps.txt
+#./Examples/Stereo/stereo_saab Vocabulary/ORBvoc.txt Examples/Stereo/SAAB.yaml ../data/left ../data/right ../data/timestamps.txt
 
 #mv CameraTrajectory.txt ../data
-mv KeyFrameTrajectory.txt ../data
+#mv KeyFrameTrajectory.txt ../data
 
-cd ..
+#cd ..
 
 #Rectify keyframe images
 
@@ -48,10 +48,8 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	printf -v number "%04d" ${number%.*}
 #	echo $number
 	fname=$number.png
-	stereo-calibration/build/undistort_rectify -l ../data/left/"$fname" -r ../data/right/"$fname" -c $3 -L ../data/lrect2/"$fname" -R ../data/rrect/"$fname"
-done < "../data/KeyFrameTrajectory.txt"
-
-cd ../../
+	stereo-calibration/build/undistort_rectify -l data/left/"$fname" -r data/right/"$fname" -c cam_stereo.yml -L data/lrect/"$fname" -R data/rrect/"$fname"
+done < "data/KeyFrameTrajectory.txt"
 
 #Create disp files
 mkdir -p data/disp
@@ -60,7 +58,7 @@ cd data/disp
 for filename in ../lrect/*.png; do
 	base=$(basename $filename) 
 	../../spsstereo/build/spsstereo ../lrect/$base ../rrect/$base  
-	mv ${base%.*}_left_disparity.png data/disp/$base
+	mv ${base%.*}_left_disparity.png $base
 done
 
 cd ../../
