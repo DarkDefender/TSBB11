b0bef4957e2c6e13534dc95b4df6bd06a52cdc0a
lucascannelloni <johli887@student.liu.se>
2016-11-23 16:54:04 +0100

    rotates point cloud before meshing

--- segmentation/planeEst.cpp
+++ segmentation/planeEst.cpp
@@ -23,11 +23,14 @@
 #include <pcl/console/parse.h>
 #include <pcl/visualization/keyboard_event.h>
 
+#include <pcl/common/transforms.h>
+
+
 int
 main (int argc, char** argv)
 {
     pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGBNormal>);
-    pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_projected(new pcl::PointCloud<pcl::PointXYZRGBNormal>);
+    pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloudTrans(new pcl::PointCloud<pcl::PointXYZRGBNormal>);
     
     pcl::PCDReader reader;
     reader.read<pcl::PointXYZRGBNormal> (argv[1], *cloud);
@@ -41,42 +44,58 @@ main (int argc, char** argv)
     // Mandatory
     seg.setModelType (pcl::SACMODEL_PLANE);
     seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setDistanceThreshold (0.5);
+    seg.setDistanceThreshold (0.05);
     
     seg.setInputCloud (cloud);
     seg.segment (*inliers, *coefficients);
     
-    std::cout << coefficients->values[0] << std::endl;
-    std::cout << coefficients->values[1] << std::endl;
-    std::cout << coefficients->values[2] << std::endl;
-    std::cout << coefficients->values[3] << std::endl;
+    //-------------
+    // Flip point cloud around plane
+    // ------------
+    Eigen::Vector3f normal(coefficients->values[0],coefficients->values[1],coefficients->values[2]);
     
-    
-    
-    
- /*   pcl::ProjectInliers<pcl::PointXYZRGBNormal> proj;
-    proj.setModelType (pcl::SACMODEL_PLANE);
-    proj.setIndices (inliers);
-    proj.setInputCloud (cloud);
-    proj.setModelCoefficients (coefficients);
-    proj.filter (*cloud_projected);
-    
-    if (inliers->indices.size () == 0)
+    if (normal(1) > 0)
     {
-        PCL_ERROR ("Could not estimate a planar model for the given dataset.");
-        return (-1);
+        normal(0) *= -1;
+        normal(1) *= -1;
+        normal(2) *= -1;
     }
-  */
+    
+    Eigen::Vector3f xy_plane_normal_vector(0,1,0);
+    Eigen::Vector3f rotation_vector = xy_plane_normal_vector.cross(normal);
+    float theta = -atan2(rotation_vector.norm(), xy_plane_normal_vector.dot(normal));
+    
+    std::cout << "Rotation vector: "<< std::endl;
+    std::cout << rotation_vector(0) << std::endl;
+    std::cout << rotation_vector(1) << std::endl;
+    std::cout << rotation_vector(2) << std::endl;
+    std::cout << "Rotation angle: "<< std::endl;
+    std::cout << theta << std::endl;
+    
+    Eigen::AngleAxisf rot(theta,rotation_vector/rotation_vector.norm());
+    Eigen::Quaternionf quat(rot);
+    Eigen::Matrix<float, 3, 1> trans;
+    trans << 0, 0, 0;
+    
+    pcl::transformPointCloudWithNormals(*cloud, *cloudTrans,trans, quat);
+    
+    // The Plane
+    pcl::ModelCoefficients plane_coeff;
+    plane_coeff.values.resize (4);
+    plane_coeff.values[0] = coefficients->values[0];
+    plane_coeff.values[1] = coefficients->values[1];
+    plane_coeff.values[2] = coefficients->values[2];
+    plane_coeff.values[3] = coefficients->values[3];
     
     // ---------------
     // Visualize
     // ---------------
+    /*
     boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
     viewer->setBackgroundColor (0, 0, 0);
-    
-    viewer->addPointCloud<pcl::PointXYZRGBNormal>(cloud,"cloud");
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGBNormal> single_color(cloud_projected, 255, 0, 0);
- //   viewer->addPointCloud<pcl::PointXYZRGBNormal>(cloud_projected,single_color, "filtCloud");
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGBNormal> single_color(cloudTrans, 255, 0, 0);
+    viewer->addPlane(plane_coeff,"plane");
+    viewer->addPointCloud<pcl::PointXYZRGBNormal>(cloudTrans,single_color,"cloud");
     
     viewer->addCoordinateSystem (1.0);
     viewer->initCameraParameters ();
@@ -85,6 +104,9 @@ main (int argc, char** argv)
         viewer->spinOnce (100);
         boost::this_thread::sleep (boost::posix_time::microseconds (100000));
     }
+    */
+    
+    pcl::io::savePCDFile (argv[1], *cloudTrans, true);
     
     return (0);
 }
